3fd2d7ba1b7e7b25013d81004fdebb8e3179c7e9
GauBen <GauBen@users.noreply.github.com>
2021-04-26 09:41:05 +0000

    deploy: 842357453d341fbe3c11239d750892ab9c4f436f

---
+++ about/resume/index.46c8a504.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){document.querySelector("#age").innerHTML=((new Date).getTime()-new Date(1999,4,7).getTime())/31536e6|0}));
+//# sourceMappingURL=index.46c8a504.js.map
