2c05e0625d573ae7d2c52db261111ec6b11cab21
GauBen <GauBen@users.noreply.github.com>
2021-04-26 09:38:21 +0000

    deploy: 0cd12ba0d00f5e40d0a3435ef2e92f0b0568341e

--- about/resume/index.ff6d9e0c.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",(function(){document.querySelector("#age").innerHTML=((new Date).getTime()-new Date(1999,4,7).getTime())/31536e6|0}));
-//# sourceMappingURL=index.ff6d9e0c.js.map
